c6a2c0e29e17468e20e199504137f0fba9874771
kaishuang1004 <u100034017@gmail.com>
2019-12-25 16:03:57 +0800

    Updates

---
+++ precache-manifest.49f0f5ac6a82032f2e9f049b22502b87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b27c6cbdbe479182180d792aff961a25",
+    "url": "/stock_analysis/index.html"
+  },
+  {
+    "revision": "2fff543f06ccd75558cc",
+    "url": "/stock_analysis/static/css/main.63c2cd36.chunk.css"
+  },
+  {
+    "revision": "b8567dc1d2bc4932acf5",
+    "url": "/stock_analysis/static/js/2.db81652e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/stock_analysis/static/js/2.db81652e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2fff543f06ccd75558cc",
+    "url": "/stock_analysis/static/js/main.f16e9d30.chunk.js"
+  },
+  {
+    "revision": "897f498a14023b2027ba",
+    "url": "/stock_analysis/static/js/runtime-main.53a9dd24.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/stock_analysis/static/media/logo.5d5d9eef.svg"
+  }
+]);

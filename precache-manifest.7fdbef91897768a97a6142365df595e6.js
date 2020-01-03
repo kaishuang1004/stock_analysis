9b468f4ab6a705166a3d3f0dbd88de0c4c860475
kaishuang1004 <u100034017@gmail.com>
2020-01-03 16:20:45 +0800

    Updates

---
+++ precache-manifest.7fdbef91897768a97a6142365df595e6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0630dfacfc9946c5a5eb22c481f7431f",
+    "url": "/stock_analysis/index.html"
+  },
+  {
+    "revision": "27d1a68a18e5575c1eaf",
+    "url": "/stock_analysis/static/css/2.bebcaf12.chunk.css"
+  },
+  {
+    "revision": "dbbd8439d540eda8746a",
+    "url": "/stock_analysis/static/css/main.af6d8fb5.chunk.css"
+  },
+  {
+    "revision": "27d1a68a18e5575c1eaf",
+    "url": "/stock_analysis/static/js/2.3bf2435e.chunk.js"
+  },
+  {
+    "revision": "efe80d506e138c343a03a1541511a2f3",
+    "url": "/stock_analysis/static/js/2.3bf2435e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dbbd8439d540eda8746a",
+    "url": "/stock_analysis/static/js/main.df34f688.chunk.js"
+  },
+  {
+    "revision": "897f498a14023b2027ba",
+    "url": "/stock_analysis/static/js/runtime-main.53a9dd24.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/stock_analysis/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/stock_analysis/precache-manifest.c3dae52c75dead612949bd2c02bd4da0.js"
+  "/stock_analysis/precache-manifest.7fdbef91897768a97a6142365df595e6.js"
 );
 
 self.addEventListener('message', (event) => {
